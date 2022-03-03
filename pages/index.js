5f90905b65776a6b36f6b5f0a9dd9b8f36e3594a
farhanmail <maill.farhanismail@gmail.com>
2022-03-03 16:18:44 +0700

    add page dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,59 @@
+import Image from "next/image";
 import Header from "../components/header";
-import styles from "../styles/Home.module.css";
+import Bg from "../assets/img/Background-asuransi.jpg";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
-      <p>Dashboard</p>
+      <div className="body">
+        <div className="content-1">
+          <h1 className="style-content-1">
+            <span className="style-content-1-a">HANNN</span> Insurance
+          </h1>
+          <div className="container">
+            <div className="style-content-1-b">
+              Mempermudah anda dalam mengurus
+            </div>
+            <div className="style-content-1-b">segala macam asuransi</div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="style-content-2">
+            <div className="row">
+              <div className="col">
+                <div className="style-content-2-a">
+                  APA ITU <span className="style-content-2-b"> ASURANSI </span>{" "}
+                  ?
+                </div>
+              </div>
+              <div className="col">
+                <div className="style-content-2-c">
+                  Asuransi adalah bentuk perjanjian antara kedua belah pihak,
+                  yaitu Tertanggung dan Penanggung, di mana Tertanggung membayar
+                  sebuah iuran kepada Penanggung demi mendapatkan bentuk ganti
+                  rugi atas risiko finansial yang dapat terjadi secara tak
+                  terduga.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="style-content-3">
+          <div className="container">
+            <div className="style-content-3-a">
+              — Manfaat <strong>ASURANSI</strong> —
+            </div>
+            <div className="style-content-3-b">
+              Fungsi Asuransi yang utama adalah untuk membantu kamu mengatasi
+              risiko tak terduga dalam hidup. Asuransi memang tidak menjamin
+              bahwa risiko tersebut akan hilang, tapi setidaknya kamu bisa
+              meminimalisir kerugian finansial yang dialami akibat risiko
+              tersebut.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
